a48b4f12ee247e222ead731e4c33b56f5995f75d
dog7350 <dog7350@gmail.com>
2023-11-15 00:33:44 +0900

    레디스 설정

--- src/main/java/kr/kro/oneaclo/www/MainApplication.java
+++ src/main/java/kr/kro/oneaclo/www/MainApplication.java
@@ -2,8 +2,12 @@ package kr.kro.oneaclo.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession
+@EnableJpaAuditing
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
