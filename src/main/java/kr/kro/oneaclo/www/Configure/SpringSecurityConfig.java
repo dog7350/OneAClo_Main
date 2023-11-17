d7ac9c881343f41e6d6d0b4a386a8b32e547d945
homework <popoy1kr@naver.com>
2023-11-17 15:50:44 +0900

    security 권한 설정

--- src/main/java/kr/kro/oneaclo/www/Configure/SpringSecurityConfig.java
+++ src/main/java/kr/kro/oneaclo/www/Configure/SpringSecurityConfig.java
@@ -1,9 +1,11 @@
 package kr.kro.oneaclo.www.Configure;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -16,6 +18,27 @@ public class SpringSecurityConfig {
     }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requet->requet
+                        .dispatcherTypeMatchers(DispatcherType.FORWARD).permitAll()
+                        .requestMatchers("common/**","/mypage/joinform","/mypage/EmailCk","views/**","/",
+                                "/mypage/join","/mypage/EmailCk","/mypage/IdCk","/mypage/EmailGetList","/mypage/SendMail","/mypage/jwtcreate").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(login->login
+                        .loginPage("/mypage/loginform")
+                        .loginProcessingUrl("/login")
+                        .usernameParameter("id")
+                        .passwordParameter("pw")
+                        .defaultSuccessUrl("/mypage/jwtcreate",true)
+                        .permitAll()
+                )
+                .logout(logout->logout
+                        .logoutUrl("/mypage/logout")
+                        .invalidateHttpSession(true)
+                        .logoutSuccessUrl("/mypage/loginform"));
+
+        http.csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 }
