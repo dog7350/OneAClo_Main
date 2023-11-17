0fc1bbd465d38042f655b5b3dbd64ede055ea2dd
dog7350 <dog7350@gmail.com>
2023-11-17 15:44:50 +0900

    스프링 시큐리티 비밀번호 비교 인증 파일 추가

---
+++ src/main/java/kr/kro/oneaclo/www/Service/Mypage/UserDetailServiceImpl.java
@@ -0,0 +1,29 @@
+package kr.kro.oneaclo.www.Service.Mypage;
+
+import kr.kro.oneaclo.www.Entity.Mypage.Members;
+import kr.kro.oneaclo.www.Repository.Mypage.MembersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+@Component
+@RequiredArgsConstructor
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final MembersRepository membersRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Members> FindUser = membersRepository.findById(username);
+        Members members = FindUser.orElseThrow(()->new UsernameNotFoundException("존재 하지 않는 회원 입니다"));
+
+        return User.builder()
+                .username(members.getId())
+                .password(members.getPw())
+                .roles("user")
+                .build();
+    }
+}
