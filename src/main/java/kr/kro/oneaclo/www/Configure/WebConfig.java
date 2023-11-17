88cfbcbd8c98f360adfad591f4b4d198c9df125f
dog7350 <dog7350@gmail.com>
2023-11-18 00:56:09 +0900

    리소스 경로 수정

--- src/main/java/kr/kro/oneaclo/www/Configure/WebConfig.java
+++ src/main/java/kr/kro/oneaclo/www/Configure/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/upload/**").addResourceLocations("file:///WebServer/upload/");
+        //registry.addResourceHandler("/upload/**").addResourceLocations("file:///WebServer/upload/");
     }
 
 }
